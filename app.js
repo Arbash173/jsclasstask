af77cbc92075ab769142c4a834e218c9236350a3
Arbash173 <arbashhumayun0@gmail.com>
2022-06-29 02:45:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// |||||||||||||||||||||||||||||||||||||||||||||||| HEAD/TAILS |||||||||||||||||||||||||||||||||||||||||||||||
+
+// var user1 = prompt("Player 1 \n Choose head/tail: ");
+// var user2 = prompt("Player 2 \n Choose head/tail: ");
+
+// var toss = Math.random();
+// toss = Math.ceil(toss * 2);
+
+// if(toss === 1){
+//   toss = "head"
+// }else{
+//   toss = "tail"
+// }
+
+// if(toss === user1 ){
+//   alert("Player1 won the toss")
+// }if(toss === user2){
+//   alert("Player2 won the toss");
+// }if(user1 === "" || user2 === ""){
+//   alert("Please choose aside.");
+// }
+
+// |||||||||||||||||||||||||||||||||||||||||||||||| DICE |||||||||||||||||||||||||||||||||||||||||||||||
+
+// var user = +prompt("How many times you want to roll dice?");
+// for(var i=0; i<user; i++){
+//     var dice = Math.random();
+//     dice = dice * 6;
+//     document.write(Math.ceil(dice) + "<br/>");
+// }
+
+// |||||||||||||||||||||||||||||||||||||||||||||||| PASSWORD GENERATOR |||||||||||||||||||||||||||||||||||||||||||||||
+
+// var str = "AbBanbj@#$njnkclk643sdmkc456s5d4cnsmnd";
+// var password = "";
+// for(var i=0; i<8; i++){
+//     var random = Math.round(Math.random()*str.length);
+//     password += str[random];
+// }
+// console.log(password);
